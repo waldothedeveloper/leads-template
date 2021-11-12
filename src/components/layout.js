8fcd00cc45012b284bbc4e24bc9b0a847cb4d7ad
Waldo Lavaut <waldothedeveloper@gmail.com>
2021-11-12 00:54:49 -0500

    I commented the goog fonts on helmet, since they're being preloaded now with the gatsby plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,12 +14,12 @@ const Layout = ({ children }) => {
           src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"
         />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Helmet>
 
       <main>
