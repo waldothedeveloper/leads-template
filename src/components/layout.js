6edfb0877aaeb1cbddd97a5e737af21ae7cde399
Waldo Lavaut <waldothedeveloper@gmail.com>
2021-11-14 22:33:51 -0500

    Removed the callrail script from my website, i dont need it

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import { Footer } from "./navigation/footer";
 import { Header } from "./navigation/header";
-import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -8,13 +7,6 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <>
-      <Helmet>
-        <script
-          async
-          type="text/javascript"
-          src="//cdn.callrail.com/companies/560693753/394c65566136d819f13c/12/swap.js"
-        ></script>
-      </Helmet>
       <main>
         <Header />
         {children}
